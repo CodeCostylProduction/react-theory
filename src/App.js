1d34684af9d80043566417c3877971ec80279c4f
CodeCostylProduction <starostin039@gmail.com>
2021-09-02 12:05:03 +0300

    up to 14 exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Car from './car/car.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        cars: [
+            {name: 'Ford', year: 2016},
+            {name: 'Mazda', year: 2018},
+            {name: 'Toyota', year: 2017}
+        ],
+        PageTitle: 'Hello world'
+    }
+
+    changeTitleHandler = () => {
+        console.log('Clicked')
+    }
+
+  render() {
+    const DivStyle = {
+      textAlign: 'center'
+    }
+    const cars = this.state.cars
+    return (
+        <div style = {DivStyle}>
+          <h1>{this.state.PageTitle}</h1>
+
+            <button onClick={}>Change title</button>
+
+            <Car name={cars[0].name} year={cars[0].year}/>
+            <Car name={cars[1].name} year={cars[1].year}/>
+            <Car name={cars[2].name} year={cars[2].year}/>
+        </div>
+    );
+  }
 }
 
 export default App;
