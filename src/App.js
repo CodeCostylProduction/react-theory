40f486c839a5870a04a04d380b6e1cc1612aa8b1
CodeCostylProduction <starostin039@gmail.com>
2021-09-04 12:55:27 +0300

    up to 21 exercise

--- src/App.js
+++ src/App.js
@@ -18,25 +18,38 @@ class App extends Component {
             showCars: !this.state.showCars
         })
     }
-
-    changeTitleHandler = (newTitle) => {
+    deleteHandler (index)  {
+        const new_cars = this.state.cars
+        new_cars.splice(index, 1)
         this.setState({
-            PageTitle: newTitle
+            cars: new_cars
         })
     }
-  render() {
+    onChangeName (name, index) {
+        const car = this.state.cars[index]
+        car.name = name
+        const new_cars = [...this.state.cars]
+        new_cars[index] = car
+        this.setState({
+            cars: new_cars
+        })
+
+    }
+
+    render() {
     const DivStyle = {
       textAlign: 'center'
     }
     let cars = null
       if (this.state.showCars === true) {
-          cars =                 this.state.cars.map((el, index) => {
+          cars = this.state.cars.map((el, index) => {
               return (
                   <Car
                       key = {index}
                       name = {el.name}
                       year = {el.year}
-                      onChangeTitle={this.changeTitleHandler.bind(this, el.name)}
+                      onDelete={this.deleteHandler.bind(this, index)}
+                      onChangeName={event => this.onChangeName(event.target.value, index)}
                   />
               )
           })}
@@ -47,7 +60,10 @@ class App extends Component {
     return (
         <div style = {DivStyle}>
           <h1>{this.state.PageTitle}</h1>
-            <button onClick={this.toggleCarsHandler}>Toggle Cars</button>
+            <button style = {{
+                marginBottom: '10px'
+            }}
+                    onClick={this.toggleCarsHandler}>Toggle Cars</button>
             { cars }
         </div>
     );
--- src/car/car.js
+++ src/car/car.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 export default (props) => (
-    <div>
+    <div style={{
+        border: '1px solid #ccc',
+        width: '400px',
+        display: 'block',
+        margin: '0 auto 10px auto'
+    }}>
         <h3>Car name: {props.name}</h3>
         <p>Year: <strong>{props.year}</strong></p>
-        <button onClick={props.onChangeTitle}>Click</button>
+        <input type="text" onChange={props.onChangeName} value={props.name}/>
+        <button onClick={props.onDelete}>Delete this</button>
     </div>
 )
