cde66c1192bb1aeaed91702ccbd636cb30dcf851
CodeCostylProduction <starostin039@gmail.com>
2021-09-05 12:49:11 +0300

    up to 24 exercise

--- src/car/car.js
+++ src/car/car.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import './car.css'
 
-export default (props) => (
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props => {
+    const inputClasses = ['input']
+
+    if (props.name !== "") {
+        inputClasses.push('green')
+        if (props.name.length > 4) {
+            inputClasses.push('bold')
+        }
+    }
+    else {
+        inputClasses.push('red')
+    }
+    return (
     <div className="Car">
         <h3>Car name: {props.name}</h3>
         <p>Year: <strong>{props.year}</strong></p>
-        <input type="text" onChange={props.onChangeName} value={props.name}/>
+        <input
+            type="text"
+            onChange={props.onChangeName}
+            value={props.name}
+            className={inputClasses.join(' ')}
+        />
         <button onClick={props.onDelete}>Delete this</button>
     </div>
-)
+)}
