56bf73f9810ce5dec19a7884cfb777c79ac8d970
CodeCostylProduction <starostin039@gmail.com>
2021-09-06 20:40:21 +0300

    up to 25 exercise

--- src/car/car.js
+++ src/car/car.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Radium from "radium";
 import './car.css'
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default props => {
+const Car = props => {
     const inputClasses = ['input']
 
     if (props.name !== "") {
@@ -14,8 +15,19 @@ export default props => {
     else {
         inputClasses.push('red')
     }
+
+    const stylesheet = {
+        border: '1px solid #ccc',
+        boxShadow: '0 4px 5px 0 rgba(0,0,0, .14)',
+        ':hover': {
+            border: '2px solid #aaa',
+            boxShadow: '0 4px 15px 0 rgba(0,0,0, .25)',
+            cursor: 'pointer'
+        }
+    }
+
     return (
-    <div className="Car">
+    <div className="Car" style={stylesheet}>
         <h3>Car name: {props.name}</h3>
         <p>Year: <strong>{props.year}</strong></p>
         <input
@@ -27,3 +39,5 @@ export default props => {
         <button onClick={props.onDelete}>Delete this</button>
     </div>
 )}
+
+export default Radium(Car)
